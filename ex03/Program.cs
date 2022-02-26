0fbdfd0407e6108acd4237c261abf59c2688439d
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-02-26 21:22:28 +0300

    ex03

---
+++ ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
