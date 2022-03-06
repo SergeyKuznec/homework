f4bd8a0f22a148037c549fa32cbcc919615bc003
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-03-06 14:10:13 +0300

    ex13

---
+++ ex13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 % number2 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("остаток " + (number1 % number2));
+}
