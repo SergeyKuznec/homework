8c87683a6d4bdf6e687725ee73e6f4417bd69c11
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-03-06 14:23:47 +0300

    ex15

---
+++ ex15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 & number % 23 == 0)
+{
+  Console.WriteLine("число одновременно кратно 7 и 23");  
+}
+else if (number % 7 == 0)
+{
+    Console.WriteLine("число кратно 7");
+}
+else if (number % 23 == 0)
+{
+    Console.WriteLine("число кратно 23");
+}
+else
+{
+    Console.WriteLine("число не кратно");
+}
