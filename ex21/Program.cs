59d909e3441ec7645068ce747518b168292fe404
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-03-08 18:27:29 +0300

    ex21

---
+++ ex21/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите координату Х первой точки");
+int X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y первой точки");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z первой точки");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Х второй точки");
+int X2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y второй точки");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z второй точки");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double dis = Math.Sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1) + (Z2 - Z1) * (Z2 - Z1));
+Console.WriteLine($"расстояние между точками равно : {dis:f3}");
