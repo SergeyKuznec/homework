b9ddcd1f94fb683d1a21ceaa124a042d8868101d
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-03-08 18:11:31 +0300

    ex19

---
+++ ex19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введител пятизначное число");
+string number = Convert.ToString(Console.ReadLine());
+bool a = true;
+for (int i =0; i < 2; i++)
+{
+    if (number[i] != number[number.Length - i -1])
+    {
+        a = false;
+        break;
+    }
+}
+if (a == true)
+{
+    Console.WriteLine("Полиндром");
+}
+else
+{
+    Console.WriteLine("Не полиндром");
+}
