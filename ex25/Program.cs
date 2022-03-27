677ea2485352294d31445a4d710fe238cb2a7be0
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-03-27 22:01:21 +0300

    ex25

---
+++ ex25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int comp = 1;
+for (int i = 1; i <= B; i++)
+{
+    comp = comp * A;
+}
+Console.WriteLine(comp);
