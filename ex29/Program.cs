21282c76673da14b11dbb5e2018dada15d7b058a
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-03-27 22:09:57 +0300

    ex29

---
+++ ex29/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[8]; 
+int len = array.Length;
+int index = 0;
+while(index < len)
+{
+    array[index] = Convert.ToInt32(Console.ReadLine());;
+    index++;
+}
+for (int i=0; i<len; i++)
+{
+Console.Write(array[i]);
+}
