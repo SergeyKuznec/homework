83e5f9b9bd975a031819efada328d2aff0d88600
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-04-02 01:42:14 +0300

    ex41

--- ex41/Program.cs
+++ ex41/Program.cs
@@ -1,9 +1,18 @@
 ﻿Console.WriteLine($"введите количество чисел которые собираетесь ввести");
 int numb = Convert.ToInt32(Console.ReadLine());
 int count = 0;
-for(int i = 0; i < numb; i++)
+int countpos(int a)
+{
+for(int i = 0; i < a; i++)
 {
     int pr = Convert.ToInt32(Console.ReadLine());
     if (pr > 0) count++;
 }
-Console.WriteLine($"количество чисел больше 0 равно: {count}");
+return count;
+}
+//for(int i = 0; i < numb; i++)
+//{
+//    int pr = Convert.ToInt32(Console.ReadLine());
+//    if (pr > 0) count++;
+//}
+Console.WriteLine($"количество чисел больше 0 равно: {countpos(numb)}");
