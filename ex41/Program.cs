3d171d57e5238cb381807f3c429fb0d2a380cff1
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-04-02 00:47:42 +0300

    ex41

---
+++ ex41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine($"введите количество чисел которые собираетесь ввести");
+int numb = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for(int i = 0; i < numb; i++)
+{
+    int pr = Convert.ToInt32(Console.ReadLine());
+    if (pr > 0) count++;
+}
+Console.WriteLine($"количество чисел больше 0 равно: {count}");
