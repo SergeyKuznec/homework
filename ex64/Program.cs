f145501ff71e299c20dac6a3f92f91fb9cc0fb34
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-04-13 22:54:28 +0300

    ex64

---
+++ ex64/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int n = int.Parse(Console.ReadLine());
+void nat(int a, int b)
+{
+    if (a > b)
+    {
+        Console.Write($"{b} ");
+        nat(a, b + 1);
+    }
+    if (a < b)
+    {
+        Console.Write($"{b} ");
+        nat(a, b - 1);
+    }
+    if (a == b)
+    {
+        Console.Write($"{b} ");
+    }
+}
+nat(m, n);
