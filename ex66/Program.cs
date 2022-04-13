4816e751fa1f93fc52373a5836e3a6e168206931
Sergey <kuznetcov.sergey1997@yandex.ru>
2022-04-13 23:04:40 +0300

    ex66

---
+++ ex66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int n = int.Parse(Console.ReadLine());
+int sum(int a, int b)
+{
+    
+    if (a > b)
+    {
+       return b + sum(a, b + 1);
+    }
+    if (a < b)
+    {
+       return a + sum(a + 1, b);
+    }
+    else return a;
+}
+Console.WriteLine($"Сумма цифр равна {sum(m, n)}");
